c653ce957614c7a419d69c0dd9703b0df28262a2
Cédric Menzi <cedric.menzi@buhlergroup.com>
2021-06-28 19:32:48 +0200

    intial commit

---
+++ source/src/Carrent/CarManagement/Api/CarDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Carrent.CarManagement.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CarDto
+    {
+    }
+}
